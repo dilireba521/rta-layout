d9947fc0047488829938050891276364583ec804
lkh <215551728@qq.com>
2024-08-13 18:58:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from "vue"
+import QuantizationImg from "@/assets/image/logo-quantization.svg"
+import TreasureFundImg from "@/assets/image/logo-treasure-fund.svg"
+
+const curUrl = window.location.origin;
+
+
+console.log(curUrl,window.location);
+const list = ref([
+  {
+    name: "量化中心",
+    img: QuantizationImg,
+    port: "5173"
+  },
+  {
+    name: "成长基金",
+    img: TreasureFundImg, 
+    port: "5174"
+  }
+])
+function jump(port) {
+  window.open(curUrl+':'+port, '_blank')
+}
+</script>
+
+<template>
+  <div class="list">
+    <div class="item"  @click="jump(item.port)" v-for="item in list">
+      <div class="item-img">
+        <img :src="item.img" alt="" />
+      </div>
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  align-items: center;
+  padding: 8px 0;
+  cursor: pointer;
+  font-size: 24px;
+}
+.item:hover {
+    background-color: #efc3941e;
+  }
+.item-img {
+  width: 200px;
+}
+</style>
